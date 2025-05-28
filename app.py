bc74a3dc0ecd223bcbbb9c7f992836caf7e42cdc
yuvamagnificent <yuvalakshmimagnificent@gmail.com>
2025-05-28 23:04:49 +0530

    Fix: bind Flask to 0.0.0.0 and use PORT env variable

--- app.py
+++ app.py
@@ -221,5 +221,11 @@ def api_report_data():
 
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
